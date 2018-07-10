87a9102b703c172058f9320b4a8ea5f0d7ef29f0
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-07-10 20:03:02 -0300

    Update _start.sh

--- _start.sh
+++ _start.sh
@@ -2,4 +2,5 @@
 #parameter -p HOSTPORT:CONTAINER_PORT
 #sudo docker run -p 5984:5984 -d -v /opt/couchdb/data:/opt/couchdb/data --net=netdb --name mycouchdb couchdb 
 #sudo docker run -p 8080:8080 -v $PWD:/home -d --net=netdb  --name nodedb node-db-server
-sudo docker run -d -p 8888:8888 --restart=unless-stopped -v /var/apps/labml:/data --name labml-notebook-cnr  labml-notebook-img
+#debianv4 file layout
+sudo docker run -d -p 8888:8888 --restart=unless-stopped -v /home/anderson/data/labml:/data --name labml-notebook-cnr  labml-notebook-img
